87a0bea8a898046def38719e76f32eaa9e1b802b
Tam Phan <doantamcdth5c@gmail.com>
2024-11-19 09:38:40 -0600

    Use backend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +14,91 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
+
+  String responseMessage = '';
+
+  // Su dung dia chi IP thich hop cho backend
+
+  String getBackendUrl() {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    } else {
+      return 'http://localhost:8080';
+    }
+  }
+
+  Future<void> sendName() async {
+    final name = controller.text;
+    controller.clear();
+
+    final backendUrl = getBackendUrl();
+
+    final url = Uri.parse('$backendUrl/api/v1/submit');
+    try {
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({'name': name}),
+          )
+          .timeout(const Duration(seconds: 10));
+
+      if (response.body.isNotEmpty) {
+        final data = json.decode(response.body);
+
+        setState(() {
+          responseMessage = data['message'];
+        });
+      } else {
+        setState(() {
+          responseMessage = 'Khong nhan duoc phan hoi tu server';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        responseMessage = 'Da xay ra loi: ${e.toString()}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ung dung full-stack flutter don gian'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(labelText: 'Ten'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: sendName, child: const Text('Gui')),
+            Text(
+              responseMessage,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
         ),
       ),
     );
