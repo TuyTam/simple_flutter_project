7e56df4d1505fa27600bdf8845de918bae9bcd2d
Tam Phan <doantamcdth5c@gmail.com>
2024-11-19 09:37:38 -0600

    Connect to backend

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
@@ -5,12 +6,31 @@ import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
-final _router = Router()
+final _router = Router(notFoundHandler: _notFoundHandler)
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/api/v1/check', _checkHandler)
+  ..get('/api/v1/echo/<message>', _echoHandler)
+  ..post('/api/v1/submit', _submitHandler);
+
+final _headers = {'Content-Type': 'application/json'};
 
 Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  return Response.ok(
+    json.encode({'message': 'Hello, World!'}),
+    headers: _headers,
+  );
+}
+
+// Ham xu ly yeu cau tai duong dan '/api/v1/check'
+Response _checkHandler(Request req) {
+  return Response.ok(
+    json.encode({'message': 'Welcome to mobile web application'}),
+    headers: _headers,
+  );
+}
+
+Response _notFoundHandler(Request req) {
+  return Response.notFound('Could not find "${req.url}" path on server');
 }
 
 Response _echoHandler(Request request) {
@@ -18,16 +38,83 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
+Future<Response> _submitHandler(Request req) async {
+  try {
+    //Doc payload tu request
+    final payload = await req.readAsString();
+
+    // Giai ma JSON tu payload
+    final data = await json.decode(payload);
+
+    // Lay gia tri 'name' tu data, ep kieu ve String? neu co
+    final name = data['name'] as String?;
+
+    // Kiem tra neu 'name' hop le
+    if (name != null && name.isNotEmpty) {
+      //Tao phan hoi chao mung
+      final response = {'message': 'Welcome $name'};
+
+      //Tra ve phan hoi voi statusCode 200 va noi dung JSON
+      return Response.ok(
+        json.encode(response),
+        headers: _headers,
+      );
+    } else {
+      //Tao phan hoi yeu cau cung cap ten
+      final response = {'message': 'Server khong nhan duoc ten cua ban'};
+
+      // Tra ve phan hoi voi statusCode 400 va noi dung JSON
+      return Response.badRequest(
+        body: json.encode(response),
+        headers: _headers,
+      );
+    }
+  } catch (e) {
+    // Xu ly ngoai le khi giai ma JSON
+    final response = {'message': 'Request is invalid. Error ${e.toString()}'};
+
+    return Response.badRequest(
+      body: json.encode(response),
+      headers: _headers,
+    );
+  }
+}
+
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final corsHeader = createMiddleware(requestHandler: (req) {
+    if (req.method == 'OPTIONS') {
+      return Response.ok('', headers: {
+        // Cho phep moi nguon try cap (trong moi truong dev). Trong moi truong production chung ta nen thay * bang domain cu the.
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      });
+    }
+    return null;
+  }, responseHandler: (res) {
+    return res.change(headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    });
+  });
+
+  // Configure a pipeline that logs requests and middleware
+  // final handler =
+  //     Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  final handler = Pipeline()
+      .addMiddleware(corsHeader)
+      .addMiddleware(logRequests())
+      .addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
+  // If env variable is not set, it will use the value from this variable. Otherwise, it will use the default value 8080.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
+  print('Server running at http://${server.address.host}:${server.port}');
 }
--- frontend/README.md
+++ frontend/README.md
@@ -27,3 +27,17 @@
 ### Step 5: Publish source code to GitHub and manage source code
 - Select `Source Control` at Side Bar and select `Publish to GitHub`.
 - Manage source code by commit, push (Sync Changes...), pull, ... from `Source Control` window.
+
+### Step 6: Develop backend and test
+2. Debug backend va kiem thu voi Postman
+3. Them Middleware xu ly CORS cho backend
+- **CORS (Cross-Origin Resource Sharing)** la mot co che bao mat duoc cac trinh duyet web su dung de ngan chan cac trang web gui yeu cau den mot domain khac voi domain cua trang hien tai. Dieu nay nham bao ve nguoi dung khoi cac cuoc tan cong CSRF (Cros-Site Request Forgery) va cac moi de doa bao mat khac.
+- **Vi sao can them CORS middleware?** Khi frontend (Flutter web) gui yeu cau HTTP den backend tren mot domain khac, trinh duyet web se chan yeu cau do vi pham chinh sach cung nguon goc (Same-Origin Policy). Cac yeu cau tu Flutter Web (chay tren localhost: 8081) den server backend (chay tren localhost: 8080) se bi chan neu server khong xu ly dung cac header CORS. Trinh duyet se gui mot yeu cau OPTIONS (Preflight Request) de kiem tra xem server co cho phep khong. Neu server khong phan hoi dung, yeu cau chinh se khong duoc gui.
+- **Giai phap:** Them Middleware xu ly CORS vao server backend de xu ly cac yeu cau OPTIONS bang cach tra ve cac header CORS can thiet. THem cac header CORS vao tat car cac phan hoi tu server de trinh duyet cho phep giao tiep giua frontend va backend.
+- Cap nhat ma nguon ham main.
+```dart
+
+```
+
+
+### Step 7: Develop frontend va tich hop he thong
